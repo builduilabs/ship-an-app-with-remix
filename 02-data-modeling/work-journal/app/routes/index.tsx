e21daff37ab0697ec8b476420061ab2e7de823bb
Sam Selikoff <sam.selikoff@gmail.com>
2023-03-02 15:35:57 -0500

    wip

---
+++ 02-data-modeling/work-journal/app/routes/index.tsx
@@ -0,0 +1,40 @@
+export default function Index() {
+  return (
+    <div className="p-10">
+      <h1 className="text-5xl">Work Journal</h1>
+      <p className="mt-2 text-lg text-gray-400">
+        Learnings and doings. Updated weekly.
+      </p>
+
+      <div className="mt-6">
+        <p className="font-bold">
+          Week of February 20<sup>th</sup>
+        </p>
+
+        <div className="mt-3 space-y-4">
+          <div>
+            <p>Work</p>
+            <ul className="ml-8 list-disc">
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+          <div>
+            <p>Learnings</p>
+            <ul className="ml-8 list-disc">
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+          <div>
+            <p>Interesting things</p>
+            <ul className="ml-8 list-disc">
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
