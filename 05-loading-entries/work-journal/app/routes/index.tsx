99f2efe7995d77e9b7153da0516162f39570395d
Sam Selikoff <sam.selikoff@gmail.com>
2023-03-28 19:41:13 -0400

    wip

---
+++ 05-loading-entries/work-journal/app/routes/index.tsx
@@ -0,0 +1,204 @@
+import { PrismaClient } from "@prisma/client";
+import { type ActionArgs } from "@remix-run/node";
+import { useFetcher, useLoaderData } from "@remix-run/react";
+import { format, parseISO, startOfWeek } from "date-fns";
+import { useEffect, useRef } from "react";
+
+export async function action({ request }: ActionArgs) {
+  let db = new PrismaClient();
+
+  let formData = await request.formData();
+  let { date, type, text } = Object.fromEntries(formData);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  if (
+    typeof date !== "string" ||
+    typeof type !== "string" ||
+    typeof text !== "string"
+  ) {
+    throw new Error("Bad request");
+  }
+
+  return db.entry.create({
+    data: {
+      date: new Date(date),
+      type: type,
+      text: text,
+    },
+  });
+}
+
+export async function loader() {
+  let db = new PrismaClient();
+  let entries = await db.entry.findMany();
+
+  return entries.map((entry) => ({
+    ...entry,
+    date: entry.date.toISOString().substring(0, 10),
+  }));
+}
+
+export default function Index() {
+  let entries = useLoaderData<typeof loader>();
+
+  let fetcher = useFetcher();
+  let textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (fetcher.state === "idle" && textareaRef.current) {
+      textareaRef.current.value = "";
+      textareaRef.current.focus();
+    }
+  }, [fetcher.state]);
+
+  let entriesByWeek = entries.reduce<Record<string, typeof entries>>(
+    (memo, entry) => {
+      let sunday = startOfWeek(parseISO(entry.date));
+      let sundayString = format(sunday, "yyyy-MM-dd");
+
+      memo[sundayString] ||= [];
+      memo[sundayString] = [...memo[sundayString], entry];
+
+      return memo;
+    },
+    {}
+  );
+
+  let weeks = Object.keys(entriesByWeek)
+    .sort((a, b) => a.localeCompare(b))
+    .map((startingDate) => ({
+      startingDate,
+      work: entriesByWeek[startingDate].filter(
+        (entry) => entry.type === "work"
+      ),
+      learnings: entriesByWeek[startingDate].filter(
+        (entry) => entry.type === "learning"
+      ),
+      interestingThings: entriesByWeek[startingDate].filter(
+        (entry) => entry.type === "interesting-thing"
+      ),
+    }));
+
+  return (
+    <div className="p-10">
+      <h1 className="text-5xl">Work Journal</h1>
+      <p className="mt-2 text-lg text-gray-400">
+        Learnings and doings. Updated weekly.
+      </p>
+
+      <div className="my-8 border p-3">
+        <p className="italic">Create a new entry</p>
+
+        <fetcher.Form method="post" className="mt-2">
+          <fieldset
+            className="disabled:opacity-70"
+            disabled={fetcher.state === "submitting"}
+          >
+            <div>
+              <div>
+                <input
+                  type="date"
+                  name="date"
+                  required
+                  className="text-gray-900"
+                  defaultValue={format(new Date(), "yyyy-MM-dd")}
+                />
+              </div>
+              <div className="mt-4 space-x-4">
+                <label className="inline-block">
+                  <input
+                    required
+                    type="radio"
+                    defaultChecked
+                    className="mr-1"
+                    name="type"
+                    value="work"
+                  />
+                  Work
+                </label>
+                <label className="inline-block">
+                  <input
+                    type="radio"
+                    className="mr-1"
+                    name="type"
+                    value="learning"
+                  />
+                  Learning
+                </label>
+                <label className="inline-block">
+                  <input
+                    type="radio"
+                    className="mr-1"
+                    name="type"
+                    value="interesting-thing"
+                  />
+                  Interesting thing
+                </label>
+              </div>
+            </div>
+            <div className="mt-4">
+              <textarea
+                ref={textareaRef}
+                placeholder="Type your entry..."
+                name="text"
+                className="w-full text-gray-700"
+                required
+              />
+            </div>
+            <div className="mt-2 text-right">
+              <button
+                type="submit"
+                className="bg-blue-500 px-4 py-1 font-semibold text-white"
+              >
+                {fetcher.state === "submitting" ? "Saving..." : "Save"}
+              </button>
+            </div>
+          </fieldset>
+        </fetcher.Form>
+      </div>
+
+      <div className="space-y-16">
+        {weeks.map((week) => (
+          <div key={week.startingDate} className="mt-6">
+            <p className="font-bold">
+              Week of {format(parseISO(week.startingDate), "MMMM do")}
+            </p>
+            <div className="mt-3 space-y-4">
+              {week.work.length > 0 && (
+                <div>
+                  <p>Work</p>
+                  <ul className="ml-8 list-disc">
+                    {week.work.map((entry) => (
+                      <li key={entry.id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {week.learnings.length > 0 && (
+                <div>
+                  <p>Learning</p>
+                  <ul className="ml-8 list-disc">
+                    {week.learnings.map((entry) => (
+                      <li key={entry.id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {week.interestingThings.length > 0 && (
+                <div>
+                  <p>Interesting things</p>
+                  <ul className="ml-8 list-disc">
+                    {week.interestingThings.map((entry) => (
+                      <li key={entry.id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
