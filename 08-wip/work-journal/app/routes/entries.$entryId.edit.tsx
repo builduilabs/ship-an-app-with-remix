79eec134ef6cf515c808eb67344ab7d1e4bf6607
Sam Selikoff <sam.selikoff@gmail.com>
2023-07-05 23:42:40 -0400

    lesson 8 diff

--- 08-wip/work-journal/app/routes/entries.$entryId.edit.tsx
+++ 08-wip/work-journal/app/routes/entries.$entryId.edit.tsx
@@ -1,31 +1,52 @@
 import { PrismaClient } from "@prisma/client";
-import type { LoaderArgs } from "@remix-run/node";
+import { redirect, type ActionArgs, type LoaderArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import EntryForm from "~/components/entry-form";
 
-export async function loader({ params }: LoaderArgs) {
-  if (typeof params.entryId !== "string") {
-    throw new Response("Not found", { status: 404 });
+export async function action({ params, request }: ActionArgs) {
+  let db = new PrismaClient();
+
+  let formData = await request.formData();
+  let { date, type, text } = Object.fromEntries(formData);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  if (
+    typeof date !== "string" ||
+    typeof type !== "string" ||
+    typeof text !== "string"
+  ) {
+    throw new Error("Bad request");
   }
 
+  await db.entry.update({
+    where: { id: +params.entryId },
+    data: {
+      date: new Date(date),
+      type: type,
+      text: text,
+    },
+  });
+
+  return redirect("/");
+}
+
+export async function loader({ params }: LoaderArgs) {
   let db = new PrismaClient();
   let entry = await db.entry.findUnique({ where: { id: +params.entryId } });
 
-  if (!entry) {
-    throw new Response("Not found", { status: 404 });
-  }
-
-  return {
-    ...entry,
-    date: entry.date.toISOString().substring(0, 10),
-  };
+  return entry;
 }
 
-export default function EditPage() {
+export default function Page() {
   let entry = useLoaderData<typeof loader>();
 
   return (
-    <div className="mt-4">
-      <p>Editing entry {entry.id}</p>
+    <div className="mt-8">
+      <p className="text-xl">Edit entry</p>
+      <div className="mt-8">
+        <EntryForm entry={entry} />
+      </div>
     </div>
   );
 }
