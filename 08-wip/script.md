689be6d7e6fe3381960d0cc495364f8ba753bdc3
Sam Selikoff <sam.selikoff@gmail.com>
2023-07-11 14:19:17 -0400

    end of 8

--- 08-wip/script.md
+++ 08-wip/script.md
@@ -6,9 +6,6 @@ Add default values.
 
 Date.
 
-```jsx
-defaultValue={format(new Date(), "yyyy-MM-dd")}
-```
 
 Still want required in case user clears.
 
---
+++ 08-wip/work-journal/app/components/entry-form.tsx
@@ -0,0 +1,84 @@
+import { useFetcher } from "@remix-run/react";
+import { useRef } from "react";
+
+export default function EntryForm({
+  entry,
+}: {
+  entry: { text: string; date: string; type: string };
+}) {
+  let fetcher = useFetcher();
+  let textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  return (
+    <fetcher.Form method="post" className="mt-2">
+      <fieldset
+        className="disabled:opacity-70"
+        disabled={fetcher.state === "submitting"}
+      >
+        <div>
+          <div>
+            <input
+              type="date"
+              name="date"
+              required
+              className="text-gray-900"
+              defaultValue={entry.date}
+            />
+          </div>
+
+          <div className="mt-4 space-x-4">
+            <label className="inline-block">
+              <input
+                required
+                type="radio"
+                className="mr-1"
+                name="type"
+                value="work"
+                defaultChecked={entry.type === "work"}
+              />
+              Work
+            </label>
+            <label className="inline-block">
+              <input
+                type="radio"
+                className="mr-1"
+                name="type"
+                value="learning"
+                defaultChecked={entry.type === "learning"}
+              />
+              Learning
+            </label>
+            <label className="inline-block">
+              <input
+                type="radio"
+                className="mr-1"
+                name="type"
+                value="interesting-thing"
+                defaultChecked={entry.type === "interesting-thing"}
+              />
+              Interesting thing
+            </label>
+          </div>
+        </div>
+        <div className="mt-4">
+          <textarea
+            ref={textareaRef}
+            placeholder="Type your entry..."
+            name="text"
+            className="w-full text-gray-700"
+            defaultValue={entry.text}
+            required
+          />
+        </div>
+        <div className="mt-2 text-right">
+          <button
+            type="submit"
+            className="bg-blue-500 px-4 py-1 font-semibold text-white"
+          >
+            {fetcher.state === "submitting" ? "Saving..." : "Save"}
+          </button>
+        </div>
+      </fieldset>
+    </fetcher.Form>
+  );
+}
