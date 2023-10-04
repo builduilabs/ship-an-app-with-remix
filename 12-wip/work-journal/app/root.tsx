9460aa770cd344a249c360ce65a687bcbda97587
Sam Selikoff <sam.selikoff@gmail.com>
2023-10-04 10:50:48 -0400

    End of lesson 12

--- 12-wip/work-journal/app/root.tsx
+++ 12-wip/work-journal/app/root.tsx
@@ -1,14 +1,24 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
+  redirect,
+  type ActionArgs,
+  type LinksFunction,
+  type MetaFunction,
+  LoaderArgs,
+} from "@remix-run/node";
+import {
+  Form,
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
 import stylesheet from "~/tailwind.css";
+import { destroySession, getSession } from "./session";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -20,7 +30,25 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export async function action({ request }: ActionArgs) {
+  let session = await getSession(request.headers.get("cookie"));
+
+  return redirect("/", {
+    headers: {
+      "Set-Cookie": await destroySession(session),
+    },
+  });
+}
+
+export async function loader({ request }: LoaderArgs) {
+  let session = await getSession(request.headers.get("cookie"));
+
+  return { session: session.data };
+}
+
 export default function App() {
+  let { session } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -29,10 +57,22 @@ export default function App() {
       </head>
       <body>
         <div className="p-10">
-          <h1 className="text-5xl">Work Journal</h1>
-          <p className="mt-2 text-lg text-gray-400">
-            Learnings and doings. Updated weekly.
-          </p>
+          <div className="flex items-start justify-between">
+            <div>
+              <h1 className="text-5xl">Work Journal</h1>
+              <p className="mt-2 text-lg text-gray-400">
+                Learnings and doings. Updated weekly.
+              </p>
+            </div>
+
+            {session.isAdmin ? (
+              <Form method="post">
+                <button>Logout</button>
+              </Form>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+          </div>
 
           <Outlet />
         </div>
