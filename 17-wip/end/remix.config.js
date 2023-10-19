1bff3922eb7617da9f1f8461d83e493cbcfaad64
Sam Selikoff <sam.selikoff@gmail.com>
2023-10-18 21:55:51 -0400

    End of Lesson 17 prep

---
+++ 17-wip/end/remix.config.js
@@ -0,0 +1,12 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  future: {
+    unstable_tailwind: true,
+    v2_errorBoundary: true,
+  },
+};
---
+++ 17-wip/end/tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.zinc,
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
